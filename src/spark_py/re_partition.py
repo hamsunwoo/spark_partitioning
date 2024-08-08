87ee5834cc85c459d93001edafc9f46924d7aa45
sunwoo <summerham22@gmail.com>
2024-08-08 13:57:56 +0900

    add pyarrow

--- src/spark_py/re_partition.py
+++ src/spark_py/re_partition.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def re_partition(load_dt, base_path='/Users/seon-u/data/movie/extract'):
+def re_partition(load_dt, base_path='/Users/seon-u/data/movie/data/extract'):
     df = pd.read_parquet(f'{base_path}/load_dt={load_dt}')
     df.to_parquet('~/data/movie/repartition', partition_cols=['load_dt','multiMovieYn', 'repNationCd'])
