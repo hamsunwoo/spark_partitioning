529b36d03e2f92b6d7973f2ecaf04c4e795ac49a
sunwoo <summerham22@gmail.com>
2024-08-08 14:47:10 +0900

    add load_dt

--- src/spark_py/re_partition.py
+++ src/spark_py/re_partition.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 def re_partition(load_dt='20150101', base_path='/Users/seon-u/data/movie/data/extract'):
     df = pd.read_parquet(f'{base_path}/load_dt={load_dt}')
-    df.to_parquet('~/data/movie/repartition', partition_cols=['multiMovieYn', 'repNationCd'])
+    df['load_dt'] = load_dt
+    df.to_parquet('~/data/movie/repartition', partition_cols=['load_dt','multiMovieYn', 'repNationCd'])
 
